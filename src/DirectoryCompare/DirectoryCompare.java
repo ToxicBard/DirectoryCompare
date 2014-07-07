1b6855a60144a63c790741a6636c7d9b8b0733c1
ToxicBard <limpbizkitfan943@yahoo.com>
2014-07-07 19:24:03 -0400

    Made some progress

--- src/DirectoryCompare/DirectoryCompare.java
+++ src/DirectoryCompare/DirectoryCompare.java
@@ -1,37 +1,112 @@
 package DirectoryCompare;
 
+import java.io.File;
 import javax.swing.JFileChooser;
 
 public class DirectoryCompare {
 
+	//TODO Implement directory saving
+	//TODO Add some sort of result organization/sorting
 	public static void main(String[] args) {
 		
-		String sourceDirectory;
-		String targetDirectory;
+		String formerDirectory;
+		String latterDirectory;
 		
 		//Allow the user to select the directories
-		sourceDirectory = chooseDirectory("Select a source directory.");
-		if(sourceDirectory != null){
-			System.out.println(sourceDirectory);
+		formerDirectory = chooseDirectory("Select the former directory.");
+		if(formerDirectory != null){
+			System.out.println(formerDirectory);
 		}
 		else{
-			System.out.println("You must select a source directory.");
+			System.out.println("You must select the former directory.");
 			return;
 		}
 		
-		targetDirectory = chooseDirectory("Select a target directory.");
-		if(targetDirectory != null){
-			System.out.println(targetDirectory);
+		latterDirectory = chooseDirectory("Select the latter directory.");
+		if(latterDirectory != null){
+			System.out.println(latterDirectory);
 		}
 		else{
-			System.out.println("You must select a target directory.");
+			System.out.println("You must select the latter directory.");
 			return;
 		}
 		
-		//TODO Compare source and target directories
+		compareDirectories(formerDirectory, latterDirectory);
 		
 	}
 	
+	private static void compareDirectories(String formerDirectory, String latterDirectory){
+		System.out.println("Checking former directory...");
+		traverseDirectory(formerDirectory, latterDirectory, true);
+		
+		System.out.println("");
+		
+		System.out.println("Checking latter directory...");
+		traverseDirectory(latterDirectory, formerDirectory, false);
+		
+	}
+	
+	private static void traverseDirectory(String sourceDir, String targetDir, boolean firstCheck){
+		File[] myFiles = new File(sourceDir).listFiles();
+		
+		traverseDirectory(myFiles, sourceDir, targetDir, firstCheck);
+		
+	}
+	
+	private static void traverseDirectory(File[] myFiles, String baseSourceDir, String baseTargetDir, boolean firstCheck){
+		FileComparisonResult fileCompare;
+		
+		for(File file : myFiles){
+			fileCompare = compareFiles(file, baseSourceDir, baseTargetDir);
+			
+			processCompareResult(fileCompare, file, firstCheck);
+			
+			if(file.isDirectory()){
+				traverseDirectory(file.listFiles(), baseSourceDir, baseTargetDir, firstCheck);
+			}
+		}
+	}
+	
+	private static void processCompareResult(FileComparisonResult compareResult, File sourceFile, boolean firstCheck){
+		switch (compareResult){
+			case doesntExist:	System.out.println("Doesn't Exist: " + sourceFile.getAbsoluteFile());
+								break;
+								
+								//This case is only relevant for the first traversal
+			case differentSize:	if(firstCheck){
+									System.out.println("Different Size: " + sourceFile.getAbsoluteFile());
+								}
+								break;
+								
+								//This case is also only relevant for the first traversal
+			case differentDate:	if(firstCheck){
+									System.out.println("Different Size: " + sourceFile.getAbsoluteFile());
+								}
+								break;
+			case match:			//Do nothing
+		}
+	}
+	
+	private enum FileComparisonResult{
+		doesntExist, differentSize, differentDate, match
+	}
+	
+	private static FileComparisonResult compareFiles(File checkFile, String baseSourceDir, String baseTargetDir){
+		String relativeFilePath = checkFile.getAbsolutePath().replaceFirst(baseSourceDir, "");
+		String targetPath = baseTargetDir + relativeFilePath;
+		File targetFile = new File(targetPath);
+		
+		if(targetFile.exists()){
+			if(checkFile.length() != targetFile.length()){
+				return FileComparisonResult.differentSize;
+			}
+			if(checkFile.lastModified() != targetFile.lastModified()){
+				return FileComparisonResult.differentDate;
+			}
+			return FileComparisonResult.match;
+		}
+		return FileComparisonResult.doesntExist;
+	}
 	
 	private static String chooseDirectory(String dialogTitle){
 		JFileChooser fileChooser = new JFileChooser();
